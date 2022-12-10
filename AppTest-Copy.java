18f4ddfdbd19ab4b0be7534ffc3684a2d70e6c5b
Nilima <siddkasnil12@gmail.com>
2022-12-10 12:35:12 +0530

    add java file-2

---
+++ AppTest-Copy.java
@@ -0,0 +1,20 @@
+package purna.cucumber_practice_aug;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
